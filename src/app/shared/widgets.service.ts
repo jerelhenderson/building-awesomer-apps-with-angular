177a3866fc99b12b9442acac025484e6c400a34b
jerelhenderson <henderson.jerel@yahoo.com>
2020-06-06 11:23:49 +0000

    Challenge 7: Add more methods to Widgets Service

--- src/app/shared/widgets.service.ts
+++ src/app/shared/widgets.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
+import { Widget } from './widget.model';
 
 const BASE_URL = 'http://0.0.0.0:3000/widgets/';
 const HEADER = {headers: new Headers({'Content-Type': 'application/json'})};
@@ -14,4 +15,23 @@ export class WidgetsService {
       .map(res => res.json());
   }
 
+  load(id) {
+    return this.http.get(`${BASE_URL}${id}`)
+      .map(res => res.json());
+  }
+
+  create(widget: Widget) {
+    return this.http.post(`${BASE_URL}`, JSON.stringify(widget), HEADER)
+      .map(res => res.json());
+  }
+
+  update(widget: Widget) {
+    return this.http.patch(`${BASE_URL}${widget.id}`, JSON.stringify(widget), HEADER)
+      .map(res => res.json());
+  }
+
+  delete(widget: Widget) {
+    return this.http.delete(`${BASE_URL}${widget.id}`)
+      .map(res => res.json());
+  }
 }
